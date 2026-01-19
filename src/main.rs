d9fed505220b0fc30bc5e1991f5152339e62c6a8
gamedungeon <gamedungeon@protonmail.com>
2026-01-18 20:03:57 -0600

    Comp Changes

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ use vexide::{
     display::{Font, Text},
     math::Angle,
     prelude::*,
-    time::LowResolutionTime,
 };
 
 #[allow(non_snake_case)]
@@ -25,15 +24,22 @@ pub mod drivetrain;
 pub mod localization;
 pub mod utils;
 
+const TRENT: bool = false;
+const BLACK: bool = false;
+
 struct Robot {
     display: Display,
     drivetrain: Drivetrain,
 
-    score_mech_left: Motor,
-    score_mech_right: Motor,
+    score_mech_wheel: Motor,
+    score_mech_top: Motor,
 
     intake_left: Motor,
     intake_right: Motor,
+
+    lift: AdiDigitalOut,
+    matchload: AdiDigitalOut,
+    flap: AdiDigitalOut,
 
     controller: Controller,
 }
@@ -44,115 +50,78 @@ impl Compete for Robot {
     }
 
     async fn driver(&mut self) {
-        // Initialize motor velocity tracking variables for motors 1 & 3 only
-        let mut lm_1_time = LowResolutionTime::now();
-        let mut lm_3_time = LowResolutionTime::now();
-        let mut rm_1_time = LowResolutionTime::now();
-        let mut rm_3_time = LowResolutionTime::now();
-
-        let mut lm_1_pos = self.drivetrain.left_motor_1.position().unwrap_or_default();
-        let mut lm_3_pos = self.drivetrain.left_motor_3.position().unwrap_or_default();
-        let mut rm_1_pos = self.drivetrain.right_motor_1.position().unwrap_or_default();
-        let mut rm_3_pos = self.drivetrain.right_motor_3.position().unwrap_or_default();
-
-        // Open file once for streaming with buffer
-        let file = OpenOptions::new()
-            .create(true)
-            .append(true)
-            .open("velocity_data.txt")
-            .expect("Unable to open velocity_data.txt");
-
-        let mut writer = BufWriter::new(file);
-
-        // Write header on first run
-        writeln!(
-            writer,
-            "timestamp,pose_timestamp,left_voltage,right_voltage,left_velocity,right_velocity,x,y,rot"
-        )
-        .expect("Unable to write header");
-
-        self.display.erase(Color::RED);
-
-        let start = Instant::now();
-
         loop {
             let state = self.controller.state().unwrap_or_default();
 
-            let lside = state.left_stick.y();
-            let rside = state.right_stick.y();
+            let lside;
+            let rside;
 
-            // Calculate motor velocities for motors 1 & 3 only
-            let lm_1_vel = self.drivetrain.motor_velocity(
-                &self.drivetrain.left_motor_1,
-                &mut lm_1_time,
-                &mut lm_1_pos,
-            );
-            let lm_3_vel = self.drivetrain.motor_velocity(
-                &self.drivetrain.left_motor_3,
-                &mut lm_3_time,
-                &mut lm_3_pos,
-            );
-            let rm_1_vel = self.drivetrain.motor_velocity(
-                &self.drivetrain.right_motor_1,
-                &mut rm_1_time,
-                &mut rm_1_pos,
-            );
-            let rm_3_vel = self.drivetrain.motor_velocity(
-                &self.drivetrain.right_motor_3,
-                &mut rm_3_time,
-                &mut rm_3_pos,
-            );
+            if TRENT {
+                lside = state.left_stick.y();
+                rside = state.right_stick.y();
+            } else {
+                let forward = state.left_stick.y();
+                let turn = state.right_stick.x();
 
-            // Calculate average velocities for each side
-            let left_avg_vel = (lm_1_vel + lm_3_vel) / 2.0;
-            let right_avg_vel = (rm_1_vel + rm_3_vel) / 2.0;
-
-            {
-                let pose = self.drivetrain.pose.borrow().clone();
-
-                // Log data with timestamp
-                let timestamp = std::time::Instant::now()
-                    .duration_since(start)
-                    .as_secs_f64();
-
-                writeln!(
-                    writer,
-                    "{:},{:},{:.2},{:.2},{:.3},{:.3},{:.2},{:.2},{:.2}",
-                    timestamp,
-                    pose.timestamp.duration_since(start).as_secs_f64(),
-                    lside * 12.0,
-                    rside * 12.0,
-                    left_avg_vel,
-                    right_avg_vel,
-                    pose.x,
-                    pose.y,
-                    pose.rot.as_degrees(),
-                )
-                .expect("Unable to write data");
-
-                // Flush buffer occasionally to ensure data is written
-                writer.flush().ok();
-
-                self.display_text(
-                    format!(
-                        "X: {:.2}, Y: {:.2}, Rot: {:.2}",
-                        pose.x,
-                        pose.y,
-                        pose.rot.as_degrees()
-                    ),
-                    [10, 10],
-                );
+                lside = forward + turn;
+                rside = forward - turn;
             }
 
             self.drivetrain.left_drive(lside * 12.0);
             self.drivetrain.right_drive(rside * 12.0);
 
-            if state.button_l1.is_pressed() {
-                self.intake(12.0);
-            } else if state.button_l2.is_pressed() {
-                self.intake(-12.0);
+            if TRENT {
+                if state.button_r1.is_pressed() {
+                    self.intake(12.0);
+                } else if state.button_r2.is_pressed() {
+                    self.intake(-12.0);
+                } else {
+                    self.intake(0.0);
+                }
+
+                if state.button_l1.is_pressed() {
+                    self.score_mech(-12.0);
+                } else if state.button_l2.is_pressed() {
+                    self.score_mech(12.0);
+                } else {
+                    self.score_mech(0.0);
+                }
             } else {
-                self.intake(0.0);
+                if state.button_l1.is_pressed() {
+                    self.intake(12.0);
+                } else if state.button_l2.is_pressed() {
+                    self.intake(-12.0);
+                } else {
+                    self.intake(0.0);
+                }
+
+                if state.button_r1.is_pressed() {
+                    self.score_mech(-12.0);
+                } else if state.button_r2.is_pressed() {
+                    self.score_mech(12.0);
+                } else {
+                    self.score_mech(0.0);
+                }
+            }
+
+            if state.button_x.is_now_pressed() {
+                self.flap.toggle().ok();
+            }
+
+            if state.button_a.is_now_pressed() {
+                self.matchload.set_low().ok();
+            }
+
+            if state.button_b.is_now_pressed() {
+                self.matchload.set_high().ok();
+            }
+
+            if state.button_up.is_now_pressed() {
+                self.lift.set_high().ok();
+            }
+
+            if state.button_down.is_now_pressed() {
+                self.lift.set_low().ok();
             }
 
             sleep(Controller::UPDATE_INTERVAL).await;
@@ -164,8 +133,11 @@ impl Robot {
     fn intake(&mut self, power: f64) {
         self.intake_left.set_voltage(power).ok();
         self.intake_right.set_voltage(power).ok();
-        self.score_mech_left.set_voltage(power).ok();
-        self.score_mech_right.set_voltage(power).ok();
+        self.score_mech_wheel.set_voltage(power).ok();
+    }
+
+    fn score_mech(&mut self, power: f64) {
+        self.score_mech_top.set_voltage(power).ok();
     }
 
     fn log_data(&mut self, data: String) {
@@ -202,47 +174,84 @@ async fn main(peripherals: Peripherals) {
         timestamp: Instant::now(),
     }));
 
-    let robot = Robot {
-        drivetrain: Drivetrain {
-            left_motor_1: Motor::new(peripherals.port_1, Gearset::Blue, Direction::Forward),
-            left_motor_2: Motor::new(peripherals.port_11, Gearset::Blue, Direction::Forward),
-            left_motor_3: Motor::new(peripherals.port_2, Gearset::Blue, Direction::Reverse),
-            left_motor_4: Motor::new(peripherals.port_12, Gearset::Blue, Direction::Reverse),
+    // Black
+    let robot = if BLACK {
+        Robot {
+            drivetrain: Drivetrain {
+                left_motor_1: Motor::new(peripherals.port_1, Gearset::Blue, Direction::Forward),
+                left_motor_2: Motor::new(peripherals.port_4, Gearset::Blue, Direction::Reverse),
+                left_motor_3: Motor::new(peripherals.port_2, Gearset::Blue, Direction::Reverse),
+                left_motor_4: Motor::new(peripherals.port_15, Gearset::Blue, Direction::Forward),
 
-            right_motor_1: Motor::new(peripherals.port_20, Gearset::Blue, Direction::Reverse),
-            right_motor_2: Motor::new(peripherals.port_10, Gearset::Blue, Direction::Reverse),
-            right_motor_3: Motor::new(peripherals.port_19, Gearset::Blue, Direction::Forward),
-            right_motor_4: Motor::new(peripherals.port_9, Gearset::Blue, Direction::Forward),
+                right_motor_1: Motor::new(peripherals.port_9, Gearset::Blue, Direction::Reverse),
+                right_motor_2: Motor::new(peripherals.port_8, Gearset::Blue, Direction::Reverse),
+                right_motor_3: Motor::new(peripherals.port_10, Gearset::Blue, Direction::Forward),
+                right_motor_4: Motor::new(peripherals.port_5, Gearset::Blue, Direction::Forward),
 
-            pose: pose.clone(),
-        },
+                pose: pose.clone(),
+            },
 
-        display: peripherals.display,
+            display: peripherals.display,
 
-        score_mech_left: Motor::new(peripherals.port_6, Gearset::Blue, Direction::Reverse),
-        score_mech_right: Motor::new(peripherals.port_5, Gearset::Blue, Direction::Forward),
+            score_mech_top: Motor::new(peripherals.port_7, Gearset::Blue, Direction::Reverse),
+            score_mech_wheel: Motor::new(peripherals.port_6, Gearset::Blue, Direction::Forward),
 
-        intake_left: Motor::new(peripherals.port_15, Gearset::Blue, Direction::Forward),
-        intake_right: Motor::new(peripherals.port_16, Gearset::Blue, Direction::Reverse),
+            intake_left: Motor::new(peripherals.port_17, Gearset::Blue, Direction::Forward),
+            intake_right: Motor::new(peripherals.port_16, Gearset::Blue, Direction::Reverse),
 
-        controller: peripherals.primary_controller,
+            lift: AdiDigitalOut::new(peripherals.adi_e),
+            matchload: AdiDigitalOut::new(peripherals.adi_a),
+            flap: AdiDigitalOut::new(peripherals.adi_f),
+
+            controller: peripherals.primary_controller,
+        }
+    } else {
+        Robot {
+            drivetrain: Drivetrain {
+                left_motor_1: Motor::new(peripherals.port_1, Gearset::Blue, Direction::Forward),
+                left_motor_2: Motor::new(peripherals.port_11, Gearset::Blue, Direction::Forward),
+                left_motor_3: Motor::new(peripherals.port_3, Gearset::Blue, Direction::Reverse),
+                left_motor_4: Motor::new(peripherals.port_4, Gearset::Blue, Direction::Reverse),
+
+                right_motor_1: Motor::new(peripherals.port_9, Gearset::Blue, Direction::Reverse),
+                right_motor_2: Motor::new(peripherals.port_10, Gearset::Blue, Direction::Reverse),
+                right_motor_3: Motor::new(peripherals.port_7, Gearset::Blue, Direction::Forward),
+                right_motor_4: Motor::new(peripherals.port_8, Gearset::Blue, Direction::Forward),
+
+                pose: pose.clone(),
+            },
+
+            display: peripherals.display,
+
+            score_mech_top: Motor::new(peripherals.port_5, Gearset::Blue, Direction::Reverse),
+            score_mech_wheel: Motor::new(peripherals.port_12, Gearset::Blue, Direction::Forward),
+
+            intake_left: Motor::new(peripherals.port_17, Gearset::Blue, Direction::Forward),
+            intake_right: Motor::new(peripherals.port_20, Gearset::Blue, Direction::Reverse),
+
+            lift: AdiDigitalOut::new(peripherals.adi_a),
+            matchload: AdiDigitalOut::new(peripherals.adi_e),
+            flap: AdiDigitalOut::new(peripherals.adi_f),
+
+            controller: peripherals.primary_controller,
+        }
     };
 
-    let mut imu_1 = InertialSensor::new(peripherals.port_14);
-    let mut imu_2 = InertialSensor::new(peripherals.port_17);
+    // let mut imu_1 = InertialSensor::new(peripherals.port_14);
+    // let mut imu_2 = InertialSensor::new(peripherals.port_12);
 
-    let tracking_wheel_1 = RotationSensor::new(peripherals.port_13, Direction::Reverse);
-    let tracking_wheel_2 = RotationSensor::new(peripherals.port_18, Direction::Forward);
+    // let tracking_wheel_1 = RotationSensor::new(peripherals.port_15, Direction::Reverse);
+    // let tracking_wheel_2 = RotationSensor::new(peripherals.port_16, Direction::Forward);
 
-    let _calib = join!(imu_1.calibrate(), imu_2.calibrate()).await;
+    // let _calib = join!(imu_1.calibrate(), imu_2.calibrate()).await;
 
-    let _odom = spawn(odom_thread(
-        imu_1,
-        imu_2,
-        tracking_wheel_1,
-        tracking_wheel_2,
-        pose,
-    ));
+    // let _odom = spawn(odom_thread(
+    //     imu_1,
+    //     imu_2,
+    //     tracking_wheel_1,
+    //     tracking_wheel_2,
+    //     pose,
+    // ));
 
     robot.compete().await;
 }
