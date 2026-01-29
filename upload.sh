40ac051639421aead6693ed3ae9db38c51be3eae
gamedungeon <gamedungeon@protonmail.com>
2026-01-29 00:24:17 -0600

    Update update.sh

--- upload.sh
+++ upload.sh
@@ -3,5 +3,5 @@
 DRIVER=$(gum choose "ty" "trent")
 ROBOT=$(gum choose "purple" "black")
 
-# RUSTFLAGS="--cfg driver=\"$DRIVER\" --cfg robot=\"$ROBOT\"" cargo-v5 v5 upload --name "${ROBOT}_${DRIVER}_control"
-RUSTFLAGS="--cfg driver=\"$DRIVER\" --cfg robot=\"$ROBOT\"" cargo build
+RUSTFLAGS="--cfg driver=\"$DRIVER\" --cfg robot=\"$ROBOT\"" cargo-v5 v5 upload --name "${ROBOT}_${DRIVER}_control"
+# RUSTFLAGS="--cfg driver=\"$DRIVER\" --cfg robot=\"$ROBOT\"" cargo build
